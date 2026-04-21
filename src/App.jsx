884aad25ffc6cf663ff5da9b71c9184d931c4343
cooper <13425143012@163.com>
2026-04-21 13:43:44 +0800

    feat: add App component with layout and styling

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { DataProvider } from './context/DataContext';
+import { FileUpload } from './components/FileUpload';
+import { CalendarPanel } from './components/CalendarPanel';
+import { ChartPanel } from './components/ChartPanel';
+import './App.css';
+
+function App() {
+  return (
+    <DataProvider>
+      <div className="app">
+        <header className="header">
+          <h1>DataLens</h1>
+          <p>消费数据可视化分析</p>
+        </header>
+        <main className="main">
+          <aside className="sidebar">
+            <FileUpload />
+            <CalendarPanel />
+          </aside>
+          <section className="content">
+            <ChartPanel />
+          </section>
+        </main>
+      </div>
+    </DataProvider>
+  );
+}
+
+export default App;
